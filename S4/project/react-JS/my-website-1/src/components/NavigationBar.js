4377f080f01ffad14db0d19cbff79c0941552f3a
phylandros <48447281+phylandros@users.noreply.github.com>
2022-07-22 01:20:13 +0700

    Update NavigationBar.js

--- S4/project/react-JS/my-website-1/src/components/NavigationBar.js
+++ S4/project/react-JS/my-website-1/src/components/NavigationBar.js
@@ -1,17 +1,50 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
 import React from "react";
 
 const NavigationBar = () => {
   return (
     <div className="bg-dark">
       <Navbar variant="dark">
-        <Container>
+        <Container fluid>
           <Navbar.Brand>BGES WITEL BEKASI</Navbar.Brand>
-          <Nav>
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>About</Nav.Link>
-            <Nav.Link>Contact Us</Nav.Link>
-          </Nav>
+
+          <Form>
+            <Row>
+              <Col>
+                <Nav>
+                  <Nav.Link className="m-2">Home</Nav.Link>
+                  <Nav.Link className="m-2">About</Nav.Link>
+                  <Nav.Link className="m-2">Contact Us</Nav.Link>
+                  <Form.Group className="m-2" controlId="formBasicUsername">
+                    <Form.Control
+                      type="username"
+                      placeholder="Enter Username"
+                    />
+                  </Form.Group>
+                  <Form.Group className="m-2" controlId="formBasicPassword">
+                    <Form.Control
+                      type="password"
+                      placeholder="Enter password"
+                    />
+                  </Form.Group>
+                  <Button className="m-2" variant="primary">
+                    Login
+                  </Button>
+                  {/* <Button className="m-2" variant="secondary">
+                    Sign Up
+                  </Button> */}
+                </Nav>
+              </Col>
+            </Row>
+          </Form>
         </Container>
       </Navbar>
     </div>
